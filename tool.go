c7c54b420f25c6032b163fe37092c0204f7551d4
yangsl <yangshengli@navinfo.com>
2019-09-12 09:59:07 +0800

    添加解压zip文件的方法

--- tool.go
+++ tool.go
@@ -1,10 +1,13 @@
 package tool
 
 import (
+	"archive/zip"
 	"bufio"
 	"fmt"
 	"io"
+	"os"
 	"os/exec"
+	"strings"
 )
 
 //Execute 执行命令
@@ -50,4 +53,56 @@ func read(rc io.ReadCloser, isOut bool, success *bool) {
 		fmt.Print(line)
 	}
 
+}
+
+//DeCompress 解压zip文件
+func DeCompress(zipFile, dest string) error {
+	reader, err := zip.OpenReader(zipFile)
+	if err != nil {
+		return err
+	}
+	defer reader.Close()
+	for _, file := range reader.File {
+		rc, err := file.Open()
+		if err != nil {
+			return err
+		}
+		defer rc.Close()
+		filename := dest + "/" + file.Name
+		err = os.MkdirAll(getDir(filename), 0755)
+		if err != nil {
+			return err
+		}
+		w, err := os.Create(filename)
+		if err != nil {
+			return err
+		}
+		defer w.Close()
+		_, err = io.Copy(w, rc)
+		if err != nil {
+			return err
+		}
+		w.Close()
+		rc.Close()
+	}
+	return nil
+}
+
+func getDir(path string) string {
+	return subString(path, 0, strings.LastIndex(path, "/"))
+}
+
+func subString(str string, start, end int) string {
+	rs := []rune(str)
+	length := len(rs)
+
+	if start < 0 || start > length {
+		panic("start is wrong")
+	}
+
+	if end < start || end > length {
+		panic("end is wrong")
+	}
+
+	return string(rs[start:end])
 }
